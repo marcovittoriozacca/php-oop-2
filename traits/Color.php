a7683ce1d7175399608ecc5053ddd5b862c512b8
marcovittoriozacca <zaccamarco01@gmail.com>
2024-03-11 14:12:12 +0100

    feat: Specific exception

--- traits/Color.php
+++ traits/Color.php
@@ -8,7 +8,9 @@ trait Color{
     }
     public function setColor($_color){
         if(!is_string($_color)){
-            throw new Exception ($_color . " deve essere una stringa!" );
+            throw new InvalidArgumentException ($_color . " deve essere una stringa!" );
+        }elseif (strlen($_color) > 30) {
+            throw new LengthException ($_color . " non può superare i 30 caratteri!" );
         }
         $this->color = $_color;
     
